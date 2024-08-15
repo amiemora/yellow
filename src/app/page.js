0e30ac482c73458219a1f9ce779f114329a13ca7
Amie  Morales <xamiemmorales@gmail.com>
2024-08-15 11:19:29 -0500

    Mobile view for landing page fixed

--- src/app/page.js
+++ src/app/page.js
@@ -33,7 +33,9 @@ const HeroSection = styled(Box)(({ theme }) => ({
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
-  height: "40vh",
+  // Dynamically calculate height
+  minHeight: "calc(40vh - 64px)", // Assuming navbar is 64px
+  paddingTop: theme.spacing(2), // Add top padding for navbar
   backgroundColor: theme.palette.background.default,
   color: theme.palette.text.primary,
   textAlign: "center",
@@ -239,62 +241,6 @@ const Home = () => {
           </IconButton>
         </Box>
       </Section>
-
-      {/* Contact Section */}
-      {/* <ContactSection>
-        <Container>
-          <Typography variant="h2" gutterBottom>
-            Contact
-          </Typography>
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={6}>
-              <TextField fullWidth label="Name" margin="normal" />
-              <TextField fullWidth label="Email" margin="normal" />
-              <TextField fullWidth label="Subject" margin="normal" />
-              <TextField
-                fullWidth
-                label="Message"
-                margin="normal"
-                multiline
-                rows={4}
-              />
-              <Button variant="contained" color="primary" sx={{ mt: 2 }}>
-                Send Message
-              </Button>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                height="100%"
-              >
-                <IconButton
-                  href="https://github.com/amiemora"
-                  target="_blank"
-                  color="inherit"
-                >
-                  <GitHub />
-                </IconButton>
-                <IconButton
-                  href="https://www.linkedin.com/in/amie-morales-101b38223/"
-                  target="_blank"
-                  color="inherit"
-                >
-                  <LinkedIn />
-                </IconButton>
-                <IconButton
-                  href="https://www.instagram.com/fullstackamie/"
-                  target="_blank"
-                  color="inherit"
-                >
-                  <Twitter />
-                </IconButton>
-              </Box>
-            </Grid>
-          </Grid>
-        </Container>
-      </ContactSection> */}
     </Box>
   );
 };
